339930e75fd11a02b9e91faa24477e05825d71aa
Hn1127 <202130442624@mail.scut.edu.cn>
2024-07-12 18:35:01 +0800

    create threadpool and test

---
+++ src/locker/lock.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <exception>
+#include <pthread.h>
+#include <semaphore.h>
+
+// 封装信号量
+class sem
+{
+public:
+    // 初始值为0的信号量
+    sem(int num = 0)
+    {
+        if (sem_init(&m_sem, 0, num) != 0)
+        {
+            throw std::exception();
+        }
+    }
+    ~sem()
+    {
+        sem_destroy(&m_sem);
+    }
+    bool wait()
+    {
+        return sem_wait(&m_sem) == 0;
+    }
+    bool post()
+    {
+        return sem_post(&m_sem) == 0;
+    }
+
+private:
+    sem_t m_sem;
+};
+
+// 封装mutex互斥锁
+class MutexLock
+{
+public:
+    MutexLock()
+    {
+        if (pthread_mutex_init(&m_mutex, NULL) != 0)
+        {
+            throw std::exception();
+        }
+    }
+    ~MutexLock()
+    {
+        pthread_mutex_destroy(&m_mutex);
+    }
+    bool lock()
+    {
+        return pthread_mutex_lock(&m_mutex) == 0;
+    }
+    bool unlock()
+    {
+        return pthread_mutex_unlock(&m_mutex) == 0;
+    }
+
+private:
+    pthread_mutex_t m_mutex;
+};
+// 封装mutex互斥锁，自动上锁+解锁
+class MutexLockGuard
+{
+public:
+    explicit MutexLockGuard(MutexLock &mutex)
+        : _mutex(mutex)
+    {
+        _mutex.lock();
+    }
+
+    ~MutexLockGuard()
+    {
+        _mutex.unlock();
+    }
+
+private:
+    MutexLock _mutex;
+};
